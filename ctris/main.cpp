02970e04145f129bbe32d9379c2bb5c190a728bf
Czz <Eason0208@163.com>
2024-10-14 13:54:19 +0800

    添加空地图绘制

--- ctris/main.cpp
+++ ctris/main.cpp
@@ -1,7 +1,48 @@
 #include <graphics.h>
 
-#define MAX_WIDTH 1280
+#define MAX_WIDTH 960
 #define MAX_HEIGHT 720
+#define CELL_LENGTH 32
+
+void drawPlayingField() {
+	// 白色外侧框线，+-1 是为了比实际地图大一圈，恰好框住
+	setlinecolor(WHITE);
+	line(
+		MAX_WIDTH / 2 - 5 * CELL_LENGTH, MAX_HEIGHT / 2 + 11 * CELL_LENGTH + 1,
+		MAX_WIDTH / 2 + 5 * CELL_LENGTH, MAX_HEIGHT / 2 + 11 * CELL_LENGTH + 1
+	);
+	line(
+		MAX_WIDTH / 2 - 5 * CELL_LENGTH - 1, MAX_HEIGHT / 2 + 11 * CELL_LENGTH + 1,
+		MAX_WIDTH / 2 - 5 * CELL_LENGTH - 1, MAX_HEIGHT / 2 - 9 * CELL_LENGTH - 1
+	);
+	line(
+		MAX_WIDTH / 2 + 5 * CELL_LENGTH + 1, MAX_HEIGHT / 2 + 11 * CELL_LENGTH + 1,
+		MAX_WIDTH / 2 + 5 * CELL_LENGTH + 1, MAX_HEIGHT / 2 - 9 * CELL_LENGTH - 1
+	);
+
+	// 深灰色网格线
+	setlinecolor(DARKGRAY);
+	for (int i = 0; i < 5; i++) {
+		line(
+			MAX_WIDTH / 2 - i * CELL_LENGTH, MAX_HEIGHT / 2 + 11 * CELL_LENGTH,
+			MAX_WIDTH / 2 - i * CELL_LENGTH, MAX_HEIGHT / 2 - 9 * CELL_LENGTH
+		);
+		line(
+			MAX_WIDTH / 2 + i * CELL_LENGTH, MAX_HEIGHT / 2 + 11 * CELL_LENGTH,
+			MAX_WIDTH / 2 + i * CELL_LENGTH, MAX_HEIGHT / 2 - 9 * CELL_LENGTH
+		);
+	}
+	for (int i = 0; i < 10; i++) {
+		line(
+			MAX_WIDTH / 2 - 5 * CELL_LENGTH, MAX_HEIGHT / 2 + i * CELL_LENGTH + 1 + CELL_LENGTH,
+			MAX_WIDTH / 2 + 5 * CELL_LENGTH, MAX_HEIGHT / 2 + i * CELL_LENGTH + 1 + CELL_LENGTH
+		);
+		line(
+			MAX_WIDTH / 2 - 5 * CELL_LENGTH, MAX_HEIGHT / 2 - i * CELL_LENGTH + 1 + CELL_LENGTH,
+			MAX_WIDTH / 2 + 5 * CELL_LENGTH, MAX_HEIGHT / 2 - i * CELL_LENGTH + 1 + CELL_LENGTH
+		);
+	}
+}
 
 int main()
 {
@@ -29,7 +70,8 @@ int main()
 		// 绘制
 		cleardevice();
 
-
+		// 绘制空地图
+		drawPlayingField();
 		// 刷新屏幕
 		FlushBatchDraw();
 
