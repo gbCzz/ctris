d4ea73de0f8436859e5ca24bbd895aa92ad75d47
Czz <Eason0208@163.com>
2024-10-15 08:01:16 +0800

    新建头文件 tgame 声明和游戏流程有关的操作

---
+++ ctris/tgame.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "tdefine.h"
+
+void singleDrop(TETROMINO_t, ROTSTATE_t, int&, int&);
+void softDrop(TETROMINO_t, ROTSTATE_t, int&, int&);
+void lock(TETROMINO_t, ROTSTATE_t, int&, int&);
