7da86488a2b4d4a7c4b423cb668b6eba062082e5
Czz <Eason0208@163.com>
2024-10-15 10:26:40 +0800

    暂时删除不必要的函数原型

--- ctris/tgame.h
+++ ctris/tgame.h
@@ -3,5 +3,3 @@
 #include "tdefine.h"
 
 bool singleDrop(TETROMINO_t, ROTSTATE_t, int&, int&, int[10][22]);
-void softDrop(TETROMINO_t, ROTSTATE_t, int&, int&, int[10][22]);
-void lock(TETROMINO_t, ROTSTATE_t, int&, int&, int[10][22]);
